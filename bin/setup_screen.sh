0da8a0fcb3497a2744ee8978828624756d47f2ca
icandothings <icandothings@gmail.com>
2016-01-16 12:43:30 -0500

    profiles for screens at work, and fixed incorrect loading symbol on startup

--- bin/setup_screen.sh
+++ bin/setup_screen.sh
@@ -1,1 +1,3 @@
 [[ `echo 'dr'` == `hostname` ]] && xrandr --output HDMI2 --auto --output VGA1 --auto --right-of HDMI2
+[[ `echo 'lr'` == `hostname` ]] && [[ `echo '2'` == `xrandr -q | grep "[^s]connected" | wc -l` ]] && xrandr --output DP-0 --rotate left --auto --output LVDS-0 --off
+[[ `echo 'lr'` == `hostname` ]] && [[ `echo '1'` == `xrandr -q | grep "[^s]connected" | wc -l` ]] && xrandr --output LVDS-0 --auto
